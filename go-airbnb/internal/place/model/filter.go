d9565f128271023478ee0b42b417031fbb11b69e
Đặng Bá Kiên <94333049+kien205214862002@users.noreply.github.com>
2023-11-30 21:41:36 +0700

    Delete go-airbnb directory

--- go-airbnb/internal/place/model/filter.go
+++
@@ -1,6 +0,0 @@
-package placemodel
-
-type Filter struct {
-	OwnerId int `json:"ownerId" form:"ownerId"`
-	CityId  int `json:"cityId" form:"cityId"`
-}
--- go-airbnb/internal/upload/model/upload.go
+++
@@ -1,9 +0,0 @@
-package uploadmodel
-
-import "go01-airbnb/pkg/common"
-
-const EntityName = "Upload"
-
-type Upload struct {
-	common.Image
-}
